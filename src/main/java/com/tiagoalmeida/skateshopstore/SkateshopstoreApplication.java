18c1d85c18841d1661a5b7d724e9913eeaf4c870
Tiago Almeida <tiago.almeida.poa@gmail.com>
2022-11-15 19:27:36 -0300

    Criando projeto

---
+++ src/main/java/com/tiagoalmeida/skateshopstore/SkateshopstoreApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoalmeida.skateshopstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkateshopstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkateshopstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoalmeida/skateshopstore/SkateshopstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoalmeida.skateshopstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkateshopstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
