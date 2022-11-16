1fee1137839acb4b6f40febebdbd9d6cff32bc98
Tiago Almeida <tiago.almeida.poa@gmail.com>
2022-11-15 22:25:25 -0300

    Criando operação de instanciação

--- src/main/java/com/tiagoalmeida/skateshopstore/SkateshopstoreApplication.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/SkateshopstoreApplication.java
@@ -1,13 +1,33 @@
 package com.tiagoalmeida.skateshopstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tiagoalmeida.skateshopstore.domain.Categoria;
+import com.tiagoalmeida.skateshopstore.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class SkateshopstoreApplication {
+public class SkateshopstoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkateshopstoreApplication.class, args);
+	}
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
