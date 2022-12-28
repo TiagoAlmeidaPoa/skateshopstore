37a2d9ac331f76a1cff523a2afaeae4d7405a55f
Tiago Almeida <tiago.almeida.poa@gmail.com>
2022-12-28 11:11:02 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/tiagoalmeida/skateshopstore/service/CategoriaService.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/service/CategoriaService.java
@@ -11,51 +11,55 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.tiagoalmeida.skateshopstore.domain.Categoria;
+import com.tiagoalmeida.skateshopstore.dto.CategoriaDTO;
 import com.tiagoalmeida.skateshopstore.repository.CategoriaRepository;
 import com.tiagoalmeida.skateshopstore.service.exceptions.DataIntegrityException;
 import com.tiagoalmeida.skateshopstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	CategoriaRepository repository;
-	
-	public Categoria find( Integer id ) {
-		
+
+	public Categoria find(Integer id) {
+
 		Optional<Categoria> categoria = repository.findById(id);
 		return categoria.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id:"+ id + ", tipo: " + Categoria.class.getName()
-				));
-		
+				"Objeto não encontrado! Id:" + id + ", tipo: " + Categoria.class.getName()));
+
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repository.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
 	}
 
 	public void delete(Integer id) {
-		try {			
+		try {
 			find(id);
 			repository.deleteById(id);
-		}catch(DataIntegrityViolationException erro) {
+		} catch (DataIntegrityViolationException erro) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
 
-	public List<Categoria> findAll() {		
+	public List<Categoria> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repository.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO catDTO) {
+		return new Categoria(catDTO.getId(), catDTO.getName());
 	}
 
 }
