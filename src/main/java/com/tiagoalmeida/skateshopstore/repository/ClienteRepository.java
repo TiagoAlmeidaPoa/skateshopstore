0e6349cf8b9c4abc00de7730310970d294f3bdbb
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-01-02 13:15:57 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/tiagoalmeida/skateshopstore/repository/ClienteRepository.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/repository/ClienteRepository.java
@@ -7,5 +7,7 @@ import com.tiagoalmeida.skateshopstore.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/tiagoalmeida/skateshopstore/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tiagoalmeida.skateshopstore.domain.Cliente;
 import com.tiagoalmeida.skateshopstore.domain.enums.TipoCliente;
 import com.tiagoalmeida.skateshopstore.dto.ClienteNewDTO;
+import com.tiagoalmeida.skateshopstore.repository.ClienteRepository;
 import com.tiagoalmeida.skateshopstore.resources.exceptions.FieldMessage;
 import com.tiagoalmeida.skateshopstore.service.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if ( aux != null ) {
+			list.add(new FieldMessage("email", "email já existente"));
+		}		
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
