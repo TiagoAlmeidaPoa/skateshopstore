a730ea96e86ad4a57fd64fe0880cedd9d6a93e88
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-01-11 20:59:24 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/tiagoalmeida/skateshopstore/config/DevConfig.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.tiagoalmeida.skateshopstore.service.DBService;
+import com.tiagoalmeida.skateshopstore.service.EmailService;
+import com.tiagoalmeida.skateshopstore.service.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		dbService.instanciateTestDatabase();
 		return true;
 
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
--- src/main/java/com/tiagoalmeida/skateshopstore/service/DBService.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/service/DBService.java
@@ -117,7 +117,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "02212356984", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "tiago.almeida.poa@gmail.com", "02212356984", TipoCliente.PESSOAFISICA);
 
 		cli1.getTelefones().addAll(Arrays.asList("33251648", "98457693"));
 
