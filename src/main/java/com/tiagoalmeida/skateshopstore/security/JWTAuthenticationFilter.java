3284129fed79f66c7197b8172bc4cf53971dcce3
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-01-19 22:35:31 -0300

    ATUALIZACAO - ajustando retorno 401 na classe JWTAuthenticationFilter

--- src/main/java/com/tiagoalmeida/skateshopstore/security/JWTAuthenticationFilter.java
+++ src/main/java/com/tiagoalmeida/skateshopstore/security/JWTAuthenticationFilter.java
@@ -4,6 +4,7 @@ package com.tiagoalmeida.skateshopstore.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -14,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -57,5 +59,26 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String token = jwtUtil.generationToken(username);
 		res.addHeader("Authorization", "Bearer " + token);
 	}
+	
+	@SuppressWarnings("unused")
+	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+		 
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json"); 
+            response.getWriter().append(json());
+        }
+        
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Não autorizado\", "
+                + "\"message\": \"Email ou senha inválidos\", "
+                + "\"path\": \"/login\"}";
+        }
+    }
 
 }
